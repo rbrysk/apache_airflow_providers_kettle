794bd2cc867d956d89d87c4164de799c78f1b723
bryskiewiczr <bryskiewiczr@protonmail.com>
2023-07-12 22:41:32 +0200

    fix/update version for last fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 """Perform the package airflow-provider-kettle-operator setup."""
 setup(
